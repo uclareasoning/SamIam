e4678d2a422f08e39fb0eb7fbbc7fb9e6de2103f
jp <jp@nowhere.com>
2023-04-21 09:07:24 -0400

    Delete package edu.ucla.belief.rcform

--- inflib/src/main/java/edu/ucla/belief/rcform/RCFormInferenceEngine.java
+++
@@ -1,9 +0,0 @@
-package edu.ucla.belief.rcform;
-
-import edu.ucla.util.*;
-
-import edu.ucla.belief.InferenceEngine;
-
-/** @author taylor curtis */
-public interface RCFormInferenceEngine extends InferenceEngine, ChangeListener
-{}
